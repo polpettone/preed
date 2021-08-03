dc0abb26f10af54bfe7a055e7b65b79e170b1387
Stefan Heiden <stefan.heiden@qudosoft.de>
2021-08-03 22:05:10 +0200

    polishing

--- cmd/adapter/web/middleware.go
+++ cmd/adapter/web/middleware.go
@@ -37,30 +37,6 @@ func (app *WebApp) logRequest(next http.Handler) http.Handler {
 	})
 }
 
-func (app *WebApp) methodOverride(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Only act on POST requests.
-		if r.Method == "POST" {
-
-			// Look in the request body and headers for a spoofed method.
-			// Prefer the value in the request body if they conflict.
-			method := r.PostFormValue("_method")
-			if method == "" {
-				method = r.Header.Get("X-HTTP-Method-Override")
-			}
-
-			// Check that the spoofed method is a valid HTTP method and
-			// update the request object accordingly.
-			if method == "PUT" || method == "PATCH" || method == "DELETE" {
-				r.Method = method
-			}
-		}
-
-		// Call the next handler in the chain.
-		next.ServeHTTP(w, r)
-	})
-}
-
 func noSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 	csrfHandler.SetBaseCookie(http.Cookie{
