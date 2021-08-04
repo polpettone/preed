44bfb8c49ddd3ab1411cf33f517b2755d1451c5e
Stefan Heiden <stefan.heiden@qudosoft.de>
2021-08-04 20:17:23 +0200

    statistics with handling cancellations

--- cmd/core/models/BookingStatistics.go
+++ cmd/core/models/BookingStatistics.go
@@ -15,8 +15,10 @@ func (o BookingStatistics) Count() int {
 func (o BookingStatistics) TotalAllocationDays() int {
 	total := 0
 	for _, b := range o.Bookings {
-		days := b.EndDate.YearDay() - b.StartDate.YearDay()
-		total += days
+		if !b.Canceled {
+			days := b.EndDate.YearDay() - b.StartDate.YearDay()
+			total += days
+		}
 	}
 	return total
 }
@@ -24,9 +26,11 @@ func (o BookingStatistics) TotalAllocationDays() int {
 func (o BookingStatistics) TotalIncome() *money.Money {
 	total := money.New(0, "EUR")
 	for _, b := range o.Bookings {
-		days := b.EndDate.YearDay() - b.StartDate.YearDay()
-		p := b.PricePerDay.Multiply(int64(days))
-		total, _ = total.Add(p)
+		if !b.Canceled {
+			days := b.EndDate.YearDay() - b.StartDate.YearDay()
+			p := b.PricePerDay.Multiply(int64(days))
+			total, _ = total.Add(p)
+		}
 	}
 	return total
 }
@@ -34,7 +38,9 @@ func (o BookingStatistics) TotalIncome() *money.Money {
 func (o BookingStatistics) TotalProvision() *money.Money {
 	total := money.New(0, "EUR")
 	for _, b := range o.Bookings {
-		total, _ = total.Add(&b.Provision)
+		if !b.Canceled {
+			total, _ = total.Add(&b.Provision)
+		}
 	}
 	return total
 }
