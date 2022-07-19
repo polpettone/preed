117682685d939ae0bf88d2c0377597aed040ccb3
Stefan Heiden <stefanheiden@gmail.com>
2022-07-19 15:53:33 +0200

    clean up

--- cmd/adapter/web/handlers.go
+++ cmd/adapter/web/handlers.go
@@ -7,7 +7,3 @@ import (
 func (app *WebApp) Home(w http.ResponseWriter, r *http.Request) {
 	app.render(w, r, "main.page.tmpl", &templateData{})
 }
-
-func (app *WebApp) ShowPriceTable(w http.ResponseWriter, r *http.Request) {
-	app.render(w, r, "price-table.page.tmpl", &templateData{})
-}
